ed4d8d2aedad26fd0544c724c4602b24ce4ce28a
Vinicius De Sousa <devvinicius11@gmail.com>
2026-05-12 10:47:33 -0300

    Fix CRUD Clinte

--- src/main/java/com/DevFlows/BarberFlow/Controlers/ClienteControler.java
+++ src/main/java/com/DevFlows/BarberFlow/Controlers/ClienteControler.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Cliente")
+@RequestMapping("/clientes")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class ClienteControler {
